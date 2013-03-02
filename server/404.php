6489be8b018ed6aa13d390ec5ae17b6f379b5a66
Razican <admin@razican.com>
2013-03-02 12:00:34 +0100

    Se crea la estructura preliminar del servidor. Hay problemas para implementar un SSL no firmado, y estoy trabajando en ello. En cuanto esté, implementaré la conexión con el servidor.

---
+++ server/404.php
@@ -0,0 +1,13 @@
+<?php header("HTTP/1.0 404 Not Found"); ?>
+<!DOCTYPE html>
+<html lang="es_ES">
+	<head>
+		<title>Página no encontrada</title>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<h1>Error 404</h1>
+		<h2>Página no encontrada</h2>
+		<p>La página que estabas buscando no ha sido encontrada, puedes volver al inicio haciendo click <a href="http://jdix.razican.com/" title="Volver a JDIX Developers">aquí</a>.</p>
+	</body>
+</html>
