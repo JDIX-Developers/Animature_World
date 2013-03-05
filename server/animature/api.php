6774779ae1dab4c80c283894e2590386694b1e41
Razican <admin@razican.com>
2013-03-05 10:02:55 +0100

    Se crean los métodos de comprobación de la API.

--- server/animature/api.php
+++ server/animature/api.php
@@ -1,1 +1,44 @@
-Hola!!!
+<?php
+
+function is_ssl()
+{
+	return ( ! empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off');
+}
+
+function is_animature()
+{
+	return (bool) preg_match('/Animature\/[1-9]+\.[0-9]+\.[0-9]+/', $_SERVER['HTTP_USER_AGENT']);
+}
+
+function cl_version()
+{
+	if ( ! is_animature())
+	{
+		return NULL;
+	}
+	else
+	{
+		static $v;
+		if (empty($v))
+		{
+			$va = array();
+			preg_match('/^Animature\/[1-9]+\.[0-9]+\.[0-9]+$/', $_SERVER['HTTP_USER_AGENT'], $va);
+			$v = explode('.', str_replace('Animature/', '', $va[0]));
+
+			foreach ($v as $key =>$int)
+			{
+				$v[$key] = (int) $int;
+			}
+		}
+		return $v;
+	}
+}
+
+//if (is_ssl() && is_animature())
+//{
+
+
+
+
+
+//}
