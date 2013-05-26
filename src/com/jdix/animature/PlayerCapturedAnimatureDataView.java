fdf2d93cfe507a1187e3b728cb28c9cb4af3cebe
Jordan Aranda Tejada <jordan.aranda@me.com>
2013-05-26 22:48:25 +0200

    Se empieza con el view del captured

---
+++ src/com/jdix/animature/PlayerCapturedAnimatureDataView.java
@@ -0,0 +1,58 @@
+package com.jdix.animature;
+
+import android.app.TabActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+
+/**
+ * @author Jordan Aranda Tejada
+ */
+@SuppressWarnings ("deprecation")
+public class PlayerCapturedAnimatureDataView extends TabActivity {
+
+	@Override
+	protected void onCreate(final Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_player_captured_animature_data_view);
+
+		final TabHost tabHost = getTabHost();
+
+		// Tab for Info Animature
+		final TabSpec infoAnim = tabHost.newTabSpec("Inf. Anim.");
+		infoAnim.setIndicator("Inf. Anim");
+		// final Intent infoAnimatureIntent = new Intent(this,
+		// PlayerCapturedInfoViewActivity.class);
+		// infoAnim.setContent(infoAnimatureIntent);
+
+		// Tab for Cualities. Anim.
+		final TabSpec habilAnim = tabHost.newTabSpec("Habil. Anim.");
+		habilAnim.setIndicator("Habil. Anim");
+		// final Intent qualitiesAnimIntent = new Intent(this,
+		// PlayerCapturedCualitiesViewAtivity.class);
+		// habilAnim.setContent(qualitiesAnimIntent);
+
+		// Tab for Attacks Anim.
+		final TabSpec attacksAnim = tabHost.newTabSpec("Attacks");
+		attacksAnim.setIndicator("Ataques");
+		// final Intent attacksAnimIntent = new Intent(this,
+		// PlayerCapturedAttacksViewIntent.class);
+		// attacksAnim.setContent(attacksAnimIntent);
+
+		// Adding all TabSpec to TabHost
+		tabHost.addTab(infoAnim);
+		tabHost.addTab(habilAnim);
+		tabHost.addTab(attacksAnim);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(final Menu menu)
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.player_captured_animature_data_view,
+		menu);
+		return true;
+	}
+}
