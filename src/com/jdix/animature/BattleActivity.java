9ec919220a346b5c493286c25e27b13b39cb768d
Jordan Aranda Tejada <jordan.aranda@me.com>
2013-05-27 06:51:06 +0200

    Se quita el back del battle

--- src/com/jdix/animature/BattleActivity.java
+++ src/com/jdix/animature/BattleActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -563,13 +562,10 @@ public class BattleActivity extends Activity {
 	}
 
 	@Override
-	public boolean onKeyDown(final int keyCode, final KeyEvent event)
+	public void onBackPressed()
 	{
-		if (keyCode == KeyEvent.KEYCODE_MENU
-		&& keyCode == KeyEvent.KEYCODE_BACK)
-		{
-			return false;
-		}
-		return super.onKeyDown(keyCode, event);
+		// do something on back.
+		return;
 	}
+
 }
